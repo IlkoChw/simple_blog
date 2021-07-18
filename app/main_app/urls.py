0d9176893cdcd93598854a626a8c1184814dcb5a
IlkoChw <ilkochw@gmail.com>
2021-07-18 17:38:06 +0300

    Заготовка под ленту

--- app/main_app/urls.py
+++ app/main_app/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from .views import FeedView
 
-urlpatterns = []
+
+urlpatterns = [
+    path('feed/', FeedView.as_view()),
+]
--- app/main_app/views.py
+++ app/main_app/views.py
@@ -1,1 +1,15 @@
+from django.views.generic import ListView
+from .models import Post, UserProfile
 
+
+class FeedView(ListView):
+    model = Post
+    template_name = "main_app/feed.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['posts'] = Post.objects.filter(
+            author__in=UserProfile.objects.get(user=self.request.user).subscriptions.all())
+
+        return context
